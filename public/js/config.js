dccc7f23c1a1a0269a5f858ee04914cf904eaba4
roudhentel <roudhentel.i.nagoy@gmail.com>
2017-08-24 14:49:25 +0800

    Project startup - Add left side bar - remove unneccesary files

---
+++ public/js/config.js
@@ -0,0 +1,226 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdleProvider, KeepaliveProvider) {
+
+    // Configure Idle settings
+    IdleProvider.idle(5); // in seconds
+    IdleProvider.timeout(120); // in seconds
+
+    $urlRouterProvider.otherwise("/main/card-search");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('main', {
+            abstract: true,
+            url: "/main",
+            templateUrl: "views/common/content.html",
+        })
+        .state('main.card-search', {
+            url: "/card-search",
+            templateUrl: "views/card-search.html",
+            data: { pageTitle: 'Card Search' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+
+                            serie: true,
+                            name: 'angular-flot',
+                            files: ['js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js',]
+                        },
+                        {
+                            name: 'angles',
+                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
+                        },
+                        {
+                            name: 'angular-peity',
+                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('card', {
+            abstract: true,
+            url: "/card",
+            templateUrl: "views/common/content.html",
+        })
+        .state('card.details', {
+            url: "/card-details",
+            templateUrl: "views/card-details.html",
+            data: { pageTitle: 'Card Details' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+
+                            serie: true,
+                            name: 'angular-flot',
+                            files: ['js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js',]
+                        },
+                        {
+                            name: 'angles',
+                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
+                        },
+                        {
+                            name: 'angular-peity',
+                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('cardholder', {
+            abstract: true,
+            url: "/cardholder",
+            templateUrl: "views/common/content.html",
+        })
+        .state('cardholder.information', {
+            url: "/cardholder-information",
+            templateUrl: "views/cardholder/information.html",
+            data: { pageTitle: 'Cardholder Information' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+
+                            serie: true,
+                            name: 'angular-flot',
+                            files: ['js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js',]
+                        },
+                        {
+                            name: 'angles',
+                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
+                        },
+                        {
+                            name: 'angular-peity',
+                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('cardholder.cardby-memberid', {
+            url: "/cardholder-cardby-memberid",
+            templateUrl: "views/cardholder/cardby-memberid.html",
+            data: { pageTitle: 'Cardholder Card by Member Id' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+
+                            serie: true,
+                            name: 'angular-flot',
+                            files: ['js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js',]
+                        },
+                        {
+                            name: 'angles',
+                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
+                        },
+                        {
+                            name: 'angular-peity',
+                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('cardprogram', {
+            abstract: true,
+            url: "/cardprogram",
+            templateUrl: "views/common/content.html",
+        })
+        .state('cardprogram.details', {
+            url: "/cardprogram-details",
+            templateUrl: "views/cardprogram/details.html",
+            data: { pageTitle: 'Card Program' }
+        })
+        .state('cardprogram.creditcard-rules', {
+            url: "/cardprogram-creditcard-rules",
+            templateUrl: "views/cardprogram/creditcard-rules.html",
+            data: { pageTitle: 'Credit Card Rules' }
+        })
+        .state('cardprogram.fraud', {
+            url: "/cardprogram-fraud",
+            templateUrl: "views/cardprogram/fraud.html",
+            data: { pageTitle: 'Fraud' }
+        })
+        .state('cardprogram.mcc-priviledges', {
+            url: "/cardprogram-mcc-priviledges",
+            templateUrl: "views/cardprogram/mcc-priviledges.html",
+            data: { pageTitle: 'MCC Priviledges' }
+        })
+        .state('cardprogram.parameters', {
+            url: "/cardprogram-parameters",
+            templateUrl: "views/cardprogram/parameters.html",
+            data: { pageTitle: 'Parameters' }
+        })
+        .state('cardprogram.restrictions', {
+            url: "/cardprogram-restrictions",
+            templateUrl: "views/cardprogram/restrictions.html",
+            data: { pageTitle: 'Restrictions' }
+        })
+        .state('cardprogram.rewards', {
+            url: "/cardprogram-rewards",
+            templateUrl: "views/cardprogram/rewards.html",
+            data: { pageTitle: 'Rewards' }
+        })
+        .state('cardprogram.services', {
+            url: "/cardprogram-services",
+            templateUrl: "views/cardprogram/services.html",
+            data: { pageTitle: 'Services' }
+        })
+        .state('logins', {
+            url: "/logins",
+            templateUrl: "views/login.html",
+            data: { pageTitle: 'Login', specialClass: 'gray-bg' }
+        })
+        .state('login_two_columns', {
+            url: "/login_two_columns",
+            templateUrl: "views/login_two_columns.html",
+            data: { pageTitle: 'Login two columns', specialClass: 'gray-bg' }
+        })
+        .state('register', {
+            url: "/register",
+            templateUrl: "views/register.html",
+            data: { pageTitle: 'Register', specialClass: 'gray-bg' }
+        })
+        .state('lockscreen', {
+            url: "/lockscreen",
+            templateUrl: "views/lockscreen.html",
+            data: { pageTitle: 'Lockscreen', specialClass: 'gray-bg' }
+        })
+        .state('forgot_password', {
+            url: "/forgot_password",
+            templateUrl: "views/forgot_password.html",
+            data: { pageTitle: 'Forgot password', specialClass: 'gray-bg' }
+        })
+        .state('errorOne', {
+            url: "/errorOne",
+            templateUrl: "views/errorOne.html",
+            data: { pageTitle: '404', specialClass: 'gray-bg' }
+        })
+        .state('errorTwo', {
+            url: "/errorTwo",
+            templateUrl: "views/errorTwo.html",
+            data: { pageTitle: '500', specialClass: 'gray-bg' }
+        });
+
+}
+angular
+    .module('inspinia')
+    .config(config)
+    .run(function ($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
